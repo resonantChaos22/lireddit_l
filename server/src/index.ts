65ec40c709f179cb9c1560baa904611240c183df
Shreyash <pandeyshreyash2201@gmail.com>
2022-07-26 03:47:13 +0530

    Added graphql and set up Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
 //import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+  const app = express();
+
+  app.get("/", (_, res) => {
+    res.send("Hello");
+  });
+  app.listen(4000, () => {
+    console.log(`Server started on localhost:4000`);
+  });
 };
 
 main().catch((err) => {
