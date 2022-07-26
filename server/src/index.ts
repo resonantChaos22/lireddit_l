98b7bd81523a738e6a76a51a0655d97fe901b1cc
Shreyash <pandeyshreyash2201@gmail.com>
2022-07-26 06:00:25 +0530

    Set up CRUD APIs for Posts using Resolvers and Graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -12,8 +14,11 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
     }),
   });
 
