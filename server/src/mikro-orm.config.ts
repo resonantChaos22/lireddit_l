04f619baa2e65b019e3f18a7e60cf96b4fb58733
Shreyash <pandeyshreyash2201@gmail.com>
2022-07-27 03:32:47 +0530

    Added User entity and the required migrations

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __dbpass__, __dbuser__, __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
     glob: "!(*.d).{js,ts}",
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lireddit",
   user: __dbuser__,
   password: __dbpass__,
