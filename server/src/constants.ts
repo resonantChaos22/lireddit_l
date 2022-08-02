e5784edffc84f8610a2c0730a3ab616ab32deb30
Shreyash <pandeyshreyash2201@gmail.com>
2022-08-03 05:25:23 +0530

    Added Logout Resolver in server

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -4,3 +4,4 @@ dotenv.config();
 export const __dbuser__ = "shreyash";
 export const __dbpass__ = process.env.DB_PASS;
 export const __prod__ = process.env.NODE_ENV === "production";
+export const COOKIE_NAME = "qid";
